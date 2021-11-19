0a4db9ee5b7d49dfdc5bf5fc75f667a625862c43
Edwin Jorge <ed.jorge1122@gmail.com>
2021-11-19 09:26:16 -0500

    variables de entorno creadas í´��

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const express = require('express')
 const { conexionDB } = require('./db/db')
 const cors = require('cors')
 const app = express()
+require('dotenv').config()
 
-
-app.listen(3000, async () => {
+app.listen(process.env.PUERTO, async () => {
   await conexionDB()
-  console.log('Servidor corriendo en el puerto 3000')
+  console.log('Servidor corriendo en el puerto ', process.env.PUERTO)
   
 })
 
