066dbf954a1ee9a222d309441e497fcf3a3a2bad
Raul <acevedog.raul@gmail.com>
2021-11-19 09:27:33 -0500

    conexion bases de datos

--- db/db.js
+++ db/db.js
@@ -1,14 +1,13 @@
-import mongoose from 'mongoose'
+const mongoose = require('mongoose')
 
 const conexionDB = async () => {
-  return await mongoose 
-  .connect(process.env.DATABASE_URL)
+  return await mongoose.connect('mongodb+srv://dbSmartDevs:ZQuIcWTk9VzRB6D1@cluster0.7op1x.mongodb.net/proyectosSmartDevs?retryWrites=true&w=majority')
   .then(() => {
     console.log('Conexion a la base de datos establecida')
   })
   .catch(err => {
-    console.log('Error conectando a la base de datos', err)
+    console.log('Erros conectando a la base de datos', err)
   })
 }
 
-export { conexionDB }
+exports.conexionDB = conexionDB
