fdceddf09b59a557d7325a0796ea62cc94a5b306
Daniela Ovalle <daniovape.com>
2021-11-19 11:28:54 -0500

    Se agrega código a usuario.js

---
+++ models/usuario/usuario.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose')
+const { Schema, model } = mongoose;
+
+
+const userSchema = new Schema({
+    correo: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: {
+        validator: (email) => {
+          return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email);
+        },
+
+        message: 'El formato del correo electrónico está malo.',
+      },
+    },
+    identificacion: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    nombre: {
+      type: String,
+      required: true,
+    },
+    apellido: {
+      type: String,
+      required: true,
+    },
+    rol: {
+      type: String,
+      required: true,
+      enum: ['ESTUDIANTE', 'LIDER', 'ADMINISTRADOR'],
+    },
+    estado: {
+      type: String,
+      enum: ['PENDIENTE', 'AUTORIZADO', 'NO_AUTORIZADO'],
+      default: 'PENDIENTE',
+    },
+  });
+  
+  const UserModel = model('User', userSchema);
+  
+  export { UserModel };
