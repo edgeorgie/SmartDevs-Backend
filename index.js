2addaddc987469d26e612627e218830f084c4ae0
Edwin Jorge <ed.jorge1122@gmail.com>
2021-11-24 09:39:10 -0500

    estructura basica para las rutas

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const { conexionDB } = require('./db/db')
 const cors = require('cors')
 const app = express()
+const Usuario = require('./routes/GestionUsuarios/index.routes')
+const morgan = require('morgan')
 require('dotenv').config()
 
 app.listen(process.env.PUERTO, async () => {
@@ -9,5 +11,10 @@ app.listen(process.env.PUERTO, async () => {
   console.log('Servidor corriendo en el puerto ', process.env.PUERTO)
   
 })
+
+app.use(express.json())
+app.use(morgan('dev'))
+app.use(express.urlencoded({ extended: false }));
+app.use(Usuario)
 
 app.use(cors())
--- models/usuario/usuario.js
+++ models/usuario/usuario.js
@@ -3,43 +3,46 @@ const { Schema, model } = mongoose;
 
 
 const userSchema = new Schema({
-    correo: {
-      type: String,
-      required: true,
-      unique: true,
-      validate: {
-        validator: (email) => {
-          return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email);
-        },
-
-        message: 'El formato del correo electrónico está malo.',
+  correo: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: (email) => {
+        return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email);
       },
+
+      message: 'El formato del correo electrónico está malo.',
     },
-    identificacion: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    nombre: {
-      type: String,
-      required: true,
-    },
-    apellido: {
-      type: String,
-      required: true,
-    },
-    rol: {
-      type: String,
-      required: true,
-      enum: ['ESTUDIANTE', 'LIDER', 'ADMINISTRADOR'],
-    },
-    estado: {
-      type: String,
-      enum: ['PENDIENTE', 'AUTORIZADO', 'NO_AUTORIZADO'],
-      default: 'PENDIENTE',
-    },
-  });
+  },
+  identificacion: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  nombre: {
+    type: String,
+    required: true,
+  },
+  apellido: {
+    type: String,
+    required: true,
+  },
+  rol: {
+    type: String,
+    required: true,
+    enum: ['ESTUDIANTE', 'LIDER', 'ADMINISTRADOR'],
+  },
+  estado: {
+    type: String,
+    enum: ['PENDIENTE', 'AUTORIZADO', 'NO_AUTORIZADO'],
+    default: 'PENDIENTE',
+    
+  }
+},
+  {
+    versionKey: false,
+})
   
-  const UserModel = model('User', userSchema);
   
-  export { UserModel };
+  module.exports = model('User', userSchema);
