7a885f5e873e8034aac6b06bf50331f41dd32661
LorenaDoria <lorenadoria0426@gmail.com>
2021-11-27 13:57:54 -0500

    Realizando cambios en los resolver y tipos del modelo proyecto

--- models/proyecto/resolvers.js
+++ models/proyecto/resolvers.js
@@ -2,13 +2,17 @@ const { ProjectModel } = require('./proyecto.js')
 
 const resolversProyecto = {
   Query: {
-    Proyectos: async (parent, args) => {
-      const proyectos = await ProjectModel.find().populate('lider').populate('avances');
+    Proyectos: async (parent, args, context) => {
+      const proyectos = await ProjectModel.find().populate([
+      { path : 'lider'},
+      { path :'avances'},
+      { path : 'inscripciones', populate: {path : 'estudiante'}},
+      ]);
       return proyectos;
     },
   },
   Mutation: {
-    crearProyecto: async (parent, args) => {
+    crearProyecto: async (parent, args, context ) => {
       const proyectoCreado = await ProjectModel.create({
         nombre: args.nombre,
         estado: args.estado,
@@ -20,6 +24,57 @@ const resolversProyecto = {
         objetivos: args.objetivos,
       });
       return proyectoCreado;
+    },
+    editarProyecto: async (parent, args) => {
+      const proyectoEditado = await ProjectModel.findOneAndUpdate(
+        args._id,
+        { ...args.editProyecto },
+        { new: true }
+      );
+      return proyectoEditado;
+    },
+    crearObjetivo: async (parent, args) => {
+      const proyectoObjetivo = await ProjectModel.findByIdAndUpdate(
+        { _id: args.idProyecto },
+        {
+          $addToSet: {
+            objetivos: {
+              ...args.campos,
+            },
+          },
+        },
+        { new: true }
+      );
+      console.log(proyectoObjetivo);
+      return proyectoObjetivo;
+    },
+    editarObjetivo: async (parent, args) => {
+      const proyectoEncontrado = await ProjectModel.findByIdAndUpdate(
+        { _id: args.idProyecto },
+        {
+          $set: {
+            [`objetivos.${args.indexObjetivo}.descripcion`]: args.campos.descripcion,
+            [`objetivos.${args.indexObjetivo}.tipo`]: args.campos.tipo,
+          },
+        },
+        { new: true }
+      );
+      return proyectoEncontrado;
+    },
+    eliminarObjetivo: async (parent, args) => {
+      const proyectoObjetivo = await ProjectModel.findByIdAndUpdate(
+        { _id: args.idProyecto },
+        {
+          $pull: {
+            objetivos: {
+              _id: args.idObjetivo,
+            },
+          },
+        },
+        { new: true }
+      );
+      console.log(proyectoObjetivo);
+      return proyectoObjetivo;
     },
   },
 };
