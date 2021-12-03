0669457f5a5bd36f79be035ccea2e33af7149c79
Luz Aida Buitrago Rodriguez <aidabuitrago77@gmail.com>
2021-12-03 10:14:56 -0500

    se agrega mutación para la edicion de Avances por estudiantes con su correspondiente linea en archivo tipos

--- models/avance/tipos.js
+++ models/avance/tipos.js
@@ -15,6 +15,7 @@ const tiposAvance = gql`
   }
   type Mutation {
     crearAvance(fecha: Date!, descripcion: String!, proyecto: String!, creadoPor: String!): Avance
+    editarAvanceEstudiante(_id: String!, descripcion: String! ): Avance
   }
 `;
 
